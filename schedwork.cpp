bff96f87da45c2a6168638fbe5a22f28f4779e11
Beverlyn Tsai <beverlyn@usc.edu>
2025-12-02 10:52:11 +0000

    wordle and schedwork completed

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,14 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool scheduleRecurse(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& workedShifts,
+    size_t day
+);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +46,67 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    // Create vectors for the days in schedule
+    for(size_t i=0; i<avail.size(); i++){
+        sched.push_back(vector<Worker_T>());
+    }
 
+    // Keep track of user shift number
+    vector<size_t> workedShifts(avail[0].size(), 0);
 
+    // Recursive backtrack with day set to 0
+    return scheduleRecurse(avail, dailyNeed, maxShifts, sched, workedShifts, 0);
 
 }
 
+// Helper recursive backtrack function
+bool scheduleRecurse(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<size_t>& workedShifts,
+    size_t day
+){
+
+    // Base case: if the days are all scheduled
+    if(day >= avail.size()){
+        return true;
+    }
+
+    // Loop through workers for the day
+    for(Worker_T w = 0; w < avail[0].size(); w++){
+        // Check if worker can work for that day
+        if(avail[day][w] == 1){
+            // Check to see if worker can work that day
+            if(workedShifts[w] < maxShifts){
+                // Check with find algorithm to make sure the worker has not been scheduled for this day already
+                if(find(sched[day].begin(), sched[day].end(), w) == sched[day].end()){
+                    // Add worker to work for this day
+                    sched[day].push_back(w);
+                    workedShifts[w]++;
+
+                    // Move onto the next day if the current day is already full with recursion
+                    if(sched[day].size() == dailyNeed){
+                        if(scheduleRecurse(avail, dailyNeed, maxShifts, sched, workedShifts, day+1)){
+                            return true;
+                        }
+                    }
+                    // Add another worker to this current day with recursion
+                    else{
+                        if(scheduleRecurse(avail, dailyNeed, maxShifts, sched, workedShifts, day)){
+                            return true;
+                        }
+                    }
+                
+                    // Backtrack
+                    sched[day].pop_back();
+                    workedShifts[w]--;
+                }
+            }
+        }
+    }
+
+    // Unable to completely schedule this day or schedule anyone at all if not empty here
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void recurse(std::string& current, size_t position, string floating, const string& in, const std::set<string>& dict, set<string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +23,74 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    // Create set results
+    std::set<std::string> results;
+
+    // Initialize current to in
+    std::string current = in;
+    // Recurse
+    recurse(current, 0, floating, in, dict, results);
+
+    return results;
+
 }
+
+// Recurse function
+void recurse(std::string& current, size_t position, string floating, const string& in, const std::set<string>& dict, set<string>& results){
+    // Base case
+    if(position == current.length()){
+        // Check if there are floating letters and if word exists in dictionary
+        if(floating.empty() && dict.find(current) != dict.end()){
+            results.insert(current);
+        }
+        return;
+    }
+
+    // Check if letter is fixed and recurse
+    if(in[position] != '-'){
+        // Insert the set letter to current
+        current[position] = in[position];
+        recurse(current, position+1, floating, in, dict, results);
+        return;
+    }
+
+    // Update the positions left based on word length and positions filled
+    size_t positionsRemaining = current.length() - position - 1;
+
+    // Loop through the floating letters
+    for(size_t i=0; i < floating.length(); i++){
+        // Check for remaining position space
+        if(floating.length() - 1 <= positionsRemaining){
+            current[position] = floating[i];
+
+            // Remove floating[i] from floating and store in a new string
+            std::string newFloating = floating.substr(0, i) + floating.substr(i+1);
+
+            // Recurse
+            recurse(current, position + 1, newFloating, in, dict, results);
+        }
+    }
+
+    // Substitute in other letters and see if they make valid words
+    if(positionsRemaining >= floating.length()){
+        for(char c = 'a'; c <= 'z'; c++){
+            // Check if this letter is not in floating
+            bool inFloating = false;
+            // Loop through and compare each letter with floating
+            for(char d : floating){
+                if(d == c){
+                    inFloating = true;
+                    break;
+                }
+            }
+            if(!inFloating){
+                current[position] = c;
+                // Recurse
+                recurse(current, position + 1, floating, in, dict, results);
+            }
+        }
+    }
+}
+
 
 // Define any helper functions here
